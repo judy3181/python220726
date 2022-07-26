7320d1ba0a6573efb673276837eb3165d8e4579c
Peach1 <babo3181@gmail.com>
2022-07-26 16:30:51 +0900

    클래스 멤버변수 추가

--- class2.py
+++ class2.py
@@ -3,11 +3,11 @@
 #1. 클래스 형식을 정의
 class Person:
     #클래스 멤버 변수(클래스 자체에 추가-주로 데이터 공유)
-
+    num_person = 0
     #초기화 매서드
     def __init__(self):
         self.name = "default name"
-        Person.num_person
+        Person.num_person += 1
     #인스턴스 매서드
     def print(self):
         print("My name is {0}".format(self.name))
@@ -20,3 +20,4 @@ p1.name = '전우치'
 #3. 매서드 호출
 p1.print()
 p2.print()
+print("인스턴스 개수:{0}".format(Person.num_person))
